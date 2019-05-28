df5f8f5652caa478964b84ff6d9531e106c3fce6
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2019-05-28 14:48:24 +0300

    Showing a Map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { Text, View, SafeAreaView } from "react-native";
+import { MapView } from "expo";
 
 export default class MapScreen extends Component {
+  state = {
+    region: {
+      latitude: 37,
+      longitude: -122,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.04
+    }
+  };
+
+  onRegionChangeComplete = region => {
+    this.setState({ region });
+  };
   render() {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-        <View>
-          <Text>Map Screen</Text>
-        </View>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={this.state.region}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </SafeAreaView>
     );
   }
