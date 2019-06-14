41e2ca6bb91b2fce434658c8b551e69929bd8f4a
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2019-06-14 16:26:32 +0300

    Added relative date formats

--- components/Card.js
+++ components/Card.js
@@ -28,7 +28,7 @@ const CardComponent = ({ job }) => (
     </View>
     <View style={styles.jobDetails}>
       <Text>{job.company}</Text>
-      <Text>{job.created_at}</Text>
+      <Text>{formatDate(job.created_at)}</Text>
     </View>
     <View style={styles.jobDetails}>
       <Text>{job.location}</Text>
@@ -36,6 +36,37 @@ const CardComponent = ({ job }) => (
     </View>
   </Card>
 );
+
+const formatDate = date => {
+  let s = (+new Date() - Date.parse(date)) / 1e3;
+  let m = s / 60;
+  let h = m / 60;
+  let d = h / 24;
+  let y = d / 365.242199;
+  let tmp;
+  const isEqual = num => (tmp = Math.round(num)) === 1;
+  const isBigger = num => num < 1.01;
+
+  return isEqual(s)
+    ? "just now"
+    : isBigger(m)
+    ? tmp + " seconds ago"
+    : isEqual(m)
+    ? "a minute ago"
+    : isBigger(h)
+    ? tmp + " minutes ago"
+    : isEqual(h)
+    ? "an hour ago"
+    : isBigger(d)
+    ? tmp + " hours ago"
+    : isEqual(d)
+    ? "yesterday"
+    : isBigger(y)
+    ? tmp + " days ago"
+    : isEqual(y)
+    ? "a year ago"
+    : tmp + " years ago";
+};
 
 const styles = {
   jobDetails: {
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -7,24 +7,6 @@ import { Card } from "react-native-elements";
 import Deck from "../components/Deck";
 
 class DeckScreen extends Component {
-  renderNoMoreCards() {
-    return (
-      <Card
-        title="No more jobs"
-        titleStyle={{
-          minHeight: 40,
-          paddingTop: 10,
-          textAlignVertical: "center"
-        }}
-      >
-        <Text style={{ marginBottom: 10 }}>
-          There is no jobs in this region for now... Search in another city or
-          try later
-        </Text>
-      </Card>
-    );
-  }
-
   render() {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
