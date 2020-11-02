93d2a61a574d5d273c880f1d372d6f37b1cfe437
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2020-11-02 16:23:04 +0200

    Fixed useNativeDriver warning

--- components/Deck.js
+++ components/Deck.js
@@ -6,7 +6,7 @@ import {
   PanResponder,
   Dimensions,
   LayoutAnimation,
-  UIManager
+  UIManager,
 } from "react-native";
 
 import { Card, Button } from "react-native-elements";
@@ -20,7 +20,7 @@ const SWIPE_OUT_DURATION = 250;
 class Deck extends Component {
   static defaultProps = {
     onSwipeRight: () => {},
-    onSwipeLeft: () => {}
+    onSwipeLeft: () => {},
   };
 
   constructor(props) {
@@ -41,7 +41,7 @@ class Deck extends Component {
         } else {
           this.resetPosition();
         }
-      }
+      },
     });
   }
 
@@ -61,7 +61,8 @@ class Deck extends Component {
     const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(this.position, {
       toValue: { x, y: 0 },
-      duration: SWIPE_OUT_DURATION
+      duration: SWIPE_OUT_DURATION,
+      useNativeDriver: false,
     }).start(() => this.onSwipeComplete(direction));
   }
 
@@ -76,14 +77,15 @@ class Deck extends Component {
 
   resetPosition() {
     Animated.spring(this.position, {
-      toValue: { x: 0, y: 0 }
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
     }).start();
   }
 
   getCardStyle() {
     const rotate = this.position.x.interpolate({
       inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
-      outputRange: ["-120deg", "0deg", "120deg"]
+      outputRange: ["-120deg", "0deg", "120deg"],
     });
 
     return { ...this.position.getLayout(), transform: [{ rotate }] };
@@ -99,7 +101,7 @@ class Deck extends Component {
           titleStyle={{
             minHeight: 40,
             paddingTop: 10,
-            textAlignVertical: "center"
+            textAlignVertical: "center",
           }}
         >
           <Text style={{ marginBottom: 10 }}>
@@ -136,7 +138,7 @@ class Deck extends Component {
             key={item.id}
             style={[
               styles.cardStyle,
-              { top: 10 * (index - currentCard), zIndex: 5 }
+              { top: 10 * (index - currentCard), zIndex: 5 },
             ]}
           >
             <CardComponent job={item} location={location} />
@@ -153,8 +155,8 @@ class Deck extends Component {
 const styles = {
   cardStyle: {
     position: "absolute",
-    width: SCREEN_WIDTH
-  }
+    width: SCREEN_WIDTH,
+  },
 };
 
 export default Deck;
